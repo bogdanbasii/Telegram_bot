4e570776918ee36334d9ebe396fbeb064e97519d
bogdanbasii <basiy.b@gmail.com>
2023-05-30 22:15:49 +0300

    Telegram Witch Bot, your daily horoscope

---
+++ tg_bot/views.py
@@ -0,0 +1,20 @@
+import requests
+from tg_bot import app
+from flask import request
+from .handlers import TelegramHandler
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+TG_BASE_URL = os.getenv("TG_BASE_URL")
+
+@app.route('/', methods=["POST"])
+def hello():
+    handler = TelegramHandler(request.json)
+    handler.handle()
+    return 'ok'
+
+
+
